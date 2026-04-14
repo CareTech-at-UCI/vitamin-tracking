dc0e8a706fd7aa5241f561f6031b2186439ca477
Nathan Ong <nathanong1217@gmail.com>
2026-04-13 20:49:04 -0700

    feat: daily nutrition view

---
+++ db/views/daily_nutrition_view.sql
@@ -0,0 +1,24 @@
+CREATE VIEW daily_nutrition_view AS
+
+SELECT m.user_id, 
+    DATE(m.consumed_at AT TIME ZONE 'UTC') AS consumed_date,
+    n.nutrient_id,
+    n.name AS nutrient_name,
+    n.symbol,
+    n.unit,
+
+    -- in case SUM ever returns NULL, we want to return 0 instead
+    COALESCE(SUM(mn.quantity), 0) AS total_quantity
+
+FROM meals m
+JOIN meal_items mi ON mi.meal_id = m.meal_id
+JOIN meal_nutrients mn ON mn.item_id = mi.item_id
+JOIN nutrients n ON n.nutrient_id = mn.nutrient_id
+
+GROUP BY
+    m.user_id,
+    DATE(m.consumed_at AT TIME ZONE 'UTC'),
+    n.nutrient_id,
+    n.name,
+    n.symbol,
+    n.unit;
