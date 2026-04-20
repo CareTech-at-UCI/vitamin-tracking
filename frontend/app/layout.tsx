d64e981054241b0ccdb53dc856721c8cbba159c9
Nathan Ong <nathanong1217@gmail.com>
2026-04-19 18:03:52 -0700

    feat: implemented vitamin dashboard page and added global fonts and colors

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,15 +1,18 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Montserrat_Alternates, Instrument_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+
+const montserrat = Montserrat_Alternates({
   subsets: ["latin"],
+  variable: "--font-montserrat",
+  weight: ["400", "600", "700"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const instrument = Instrument_Sans({
   subsets: ["latin"],
+  variable: "--font-instrument",
+  weight: ["400", "500", "600"],
 });
 
 export const metadata: Metadata = {
@@ -25,7 +28,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${montserrat.variable} ${instrument.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
---
+++ frontend/components/VitaminRing.jsx
@@ -0,0 +1,65 @@
+"use client";
+
+export default function VitaminRing({
+  label = "Vitamin A",
+  percent = 0,
+  onClick,
+}) {
+  const size = 110;
+  const strokeWidth = 10;
+  const radius = (size - strokeWidth) / 2;
+  const circumference = 2 * Math.PI * radius;
+  const offset = circumference - (percent / 100) * circumference;
+
+  return (
+    <div className="flex flex-col items-center gap-2 ">
+      <div className="relative">
+        <svg
+          width={size}
+          height={size}
+          viewBox={`0 0 ${size} ${size}`}
+          className="-rotate-180 text-secondary"
+                  >
+          <circle
+            cx={size / 2}
+            cy={size / 2}
+            r={radius}
+            fill="none"
+            stroke="#A9C9A4" 
+            strokeWidth={strokeWidth}
+          />
+
+          <circle
+            cx={size / 2}
+            cy={size / 2}
+            r={radius}
+            fill="none"
+            stroke="currentColor"
+            strokeWidth={strokeWidth}
+            strokeDasharray={circumference}
+            strokeDashoffset={offset}
+            strokeLinecap="round"
+          />
+        </svg>
+
+        {/* Center percentage */}
+        <div className="absolute inset-0 flex items-center justify-center">
+          <span className="text-md font-secondary font-medium text-secondary">
+            {percent}%
+          </span>
+        </div>
+      </div>
+
+      {/* Label + info */}
+      <div className="flex items-center gap-1 text-secondary">
+        <span className="text-md font-secondary font-medium">{label}</span>
+        <button
+          onClick={onClick}
+          className="text-xl font-secondary text-secondary hover:text-accent transition-colors cursor-pointer"
+        >
+          &#9432;
+        </button>
+      </div>
+    </div>
+  );
+}
