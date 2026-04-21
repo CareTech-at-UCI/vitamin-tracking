514a1fc87d16bc3be37f2147562318ac1f9daaa3
Arapaimace <adarshv06.us@gmail.com>
2026-04-21 01:28:47 -0700

    skeleton + a little progress

---
+++ frontend/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { VitaminGoals } from "@/components/dashboard";
+
+export default function Dashboard() {
+    return (
+        <div>
+            <p> Dashboard </p>
+            <VitaminGoals></VitaminGoals>
+        </div> 
+    )
+}
---
+++ frontend/components/dashboard/VitaminGoals.tsx
@@ -0,0 +1,10 @@
+export function VitaminGoals(){
+    return (
+        <div>
+            <p> Vitamin Goals </p>
+            <form>
+                <input type="search" placeholder="Search"/>
+            </form>
+        </div>
+    )
+}
