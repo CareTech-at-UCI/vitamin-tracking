5488b637ff80fac4550cf86a400715dcc1a2475d
Leo Siu <leonardoy.siu@gmail.com>
2026-04-23 01:26:05 -0700

    update: delete user meals table + normalize id for other tables

--- db/schema/user_meals.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE user_meals (
-    -- FOREIGN KEY REFERENCES
-  user_id UUID REFERENCES users(id) ON DELETE CASCADE,
-  meal_id INTEGER REFERENCES meals(meal_id) ON DELETE CASCADE,
-
-
-  -- PRIMARY KEY: (user_id, nutrient_id)
-  PRIMARY KEY (user_id, meal_id)
-)
