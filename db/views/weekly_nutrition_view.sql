3ff6a4f22bf3996b98b594324c07fdb5cbc84e3f
Nathan Ong <164730140+NoNathan17@users.noreply.github.com>
2026-04-26 14:53:25 -0700

    feat/daily-weekly-sql-views (nathan)
    * feat: daily nutrition view
    
    * feat: weekly nutrition view
    
    * fix: update views to account for updated db schema
    
    * feat: ran migration to re-add daily + weekly views
    
    ---------
    
    Co-authored-by: Nathan Ong <164730140+NoNathan17@users.noreply.github.com>
    Co-authored-by: Leo Siu <leonardoy.siu@gmail.com>

---
+++ db/views/weekly_nutrition_view.sql
@@ -0,0 +1,38 @@
+DROP VIEW IF EXISTS weekly_nutrition_view;
+
+CREATE OR REPLACE VIEW weekly_nutrition_view AS
+
+SELECT
+    m.user_id,
+
+    -- start of the week (sunday)
+    DATE_TRUNC('week', m.consumed_at AT TIME ZONE 'UTC')::DATE - INTERVAL '1 day' AS week_start,
+
+    -- end of the week (saturday)
+    DATE_TRUNC('week', m.consumed_at AT TIME ZONE 'UTC')::DATE - INTERVAL '1 day'
+    + INTERVAL '6 days' AS week_end,
+
+    n.id,
+    n.name AS nutrient_name,
+    n.symbol,
+    n.unit,
+
+    COALESCE(SUM(mn.quantity), 0) AS total_quantity
+
+FROM meals m
+JOIN meal_items mi ON mi.meal_id = m.id
+JOIN meal_nutrients mn ON mn.item_id = mi.id
+JOIN nutrients n ON n.id = mn.nutrient_id
+
+GROUP BY
+    m.user_id,
+    week_start,
+    week_end,
+    n.id,
+    n.name,
+    n.symbol,
+    n.unit
+
+ORDER BY
+  week_start DESC,
+  n.name ASC;
