611e37c239e051ce0a9278b0a873d04abd8361b1
Leo Siu <leonardoy.siu@gmail.com>
2026-05-05 01:19:46 -0700

    feat: create psql trigger to create public.users profile when auth.users is created

--- frontend/components/login/GoogleOAuth.tsx
+++ frontend/components/login/GoogleOAuth.tsx
@@ -11,8 +11,8 @@ export function GoogleOAuth() {
             provider: 'google',
             options: {
                 queryParams: {
-                access_type: 'offline',
-                prompt: 'consent',
+                    access_type: 'offline',
+                    prompt: 'consent',
                 },
             },
         });
@@ -26,7 +26,7 @@ export function GoogleOAuth() {
         <button
           type="button"
           onClick={handleSignInWithGoogle}
-          className="mt-3 inline-flex h-12 w-full items-center justify-center gap-3 rounded-xl border border-zinc-300 bg-white px-5 text-[1.25rem] font-medium text-zinc-600 shadow-sm transition hover:bg-zinc-50 lg:w-auto lg:min-w-[16rem]"
+          className="cursor-pointer mt-3 inline-flex h-12 w-full items-center justify-center gap-3 rounded-xl border border-zinc-300 bg-white px-5 text-[1.25rem] font-medium text-zinc-600 shadow-sm transition hover:bg-zinc-50 lg:w-auto lg:min-w-[16rem]"
         >
           <span className="inline-flex size-6 items-center justify-center leading-none">
              <Image src="/google.png" alt="" width={20} height={20} aria-hidden="true" />
