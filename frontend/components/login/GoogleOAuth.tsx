0c218b8f5a51dea573163c40ebba012ce20b2d6c
Leo Siu <leonardoy.siu@gmail.com>
2026-05-05 20:08:33 -0700

    feat: redirect to onboarding after sign in with google

--- frontend/components/login/GoogleOAuth.tsx
+++ frontend/components/login/GoogleOAuth.tsx
@@ -14,6 +14,7 @@ export function GoogleOAuth() {
                     access_type: 'offline',
                     prompt: 'consent',
                 },
+                redirectTo: `${window.location.origin}/onboarding`,
             },
         });
 
