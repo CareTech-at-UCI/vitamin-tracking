891347279861d19a0121d0efbadecdce1fdd5290
Arapaimace <adarshv06.us@gmail.com>
2026-08-20 14:51:40 -0700

    height/weight

--- frontend/app/profile/page.tsx
+++ frontend/app/profile/page.tsx
@@ -4,11 +4,22 @@ import PersonalInfoCard from "./_components/PersonalInfoCard";
 import DietaryRestrictionCard from "./_components/DietaryRestrictionCard";
 import DietaryPlanCard from "./_components/DietaryPlanCard";
 
-
 export default function Profile() {
     return (
         <div className = "flex flex-col items-flex-start">
-            
+            <div className = "flex flex-row items-center">
+                <HeightWeightCard
+                    title="Height"
+                    imperial={`5' 11"`}
+                    metric="180.3 cm"
+                />
+
+                <HeightWeightCard
+                    title="Weight"
+                    imperial={165.4}
+                    metric="75 kg"
+                />
+            </div>
         </div>
     );
 }
